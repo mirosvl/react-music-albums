f2b994915ba68fce3d68ac54771fb42b5f5c06e9
mirosvl <mmksav97@gmail.com>
2025-11-16 01:51:34 +0200

    vite.config.js fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import fs from "fs";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync("./cert/key.pem"),
-      cert: fs.readFileSync("./cert/cert.pem"),
-    },
-    port: 5173,
-  },
-});
+    port: 3000
+    // Remove https key/cert for deployment
+  }
+})
